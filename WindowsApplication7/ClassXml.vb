b1cd4da6baff77042a6074c75f93d272996f4b01
yourbroda <yourbroda@gmail.com>
2015-11-12 14:12:05 +0100

    fin

---
+++ WindowsApplication7/ClassXml.vb
@@ -0,0 +1,408 @@
+﻿Imports System.Xml
+Public Class ClassXml
+    Dim documento As New XmlDocument
+    Public Property nombre As String
+        Get
+            nombre = nombre1
+
+        End Get
+        Set(value As String)
+            nombre1 = value
+        End Set
+    End Property
+    Private nombre1 As String
+    Public Property ape As String
+        Get
+            ape = apellidos
+        End Get
+        Set(value As String)
+            apellidos = value
+        End Set
+    End Property
+    Dim apellidos As String
+    Public Property SS As String
+        Get
+            SS = numeroSS
+
+        End Get
+        Set(value As String)
+            numeroSS = value
+        End Set
+    End Property
+    Dim numeroSS As String
+    Public Property tlff As String
+        Get
+            tlff = tlfFijoS
+
+        End Get
+        Set(value As String)
+            tlfFijoS = value
+        End Set
+    End Property
+    Dim tlfFijoS As String
+    Public Property tlfm As String
+        Get
+            tlfm = tlfMovilS
+
+        End Get
+        Set(value As String)
+            tlfMovilS = value
+
+        End Set
+    End Property
+    Dim tlfMovilS As String
+
+    Dim estructura As String
+
+    Dim listaEmpleados As XmlNodeList = Nothing
+    Dim unEmpleado As XmlNode
+    Dim cont As Integer = 0
+
+    Public Sub abrirArchivo(ruta As String)
+
+
+        documento.Load(ruta)
+
+        Try
+            unEmpleado = listaEmpleados.Item(cont)
+
+            'Seleccionamos el id del empleado.
+            Dim idEmpleado As Integer = Convert.ToInt32(unEmpleado.SelectSingleNode("idEmpleado").InnerText)
+
+
+
+
+
+
+
+            nombre = unEmpleado.SelectSingleNode("nombre").InnerText
+
+
+            apellidos = unEmpleado.SelectSingleNode("apellidos").InnerText
+
+
+            numeroSS = unEmpleado.SelectSingleNode("numeroSS").InnerText
+
+
+            Dim telefonos As XmlNodeList = unEmpleado.SelectSingleNode("telefonos").ChildNodes
+
+
+            Dim tlfFijo As XmlNode = telefonos(0)
+            Dim tlfMovil As XmlNode = telefonos(1)
+
+            tlfFijoS = tlfFijo.InnerText
+            tlfMovilS = tlfMovil.InnerText
+
+        Catch ex As Exception
+            cont = 0
+            MsgBox("No hay datos")
+
+            unEmpleado = listaEmpleados.Item(cont)
+
+            'Seleccionamos el id del empleado.
+            Dim idEmpleado As Integer = Convert.ToInt32(unEmpleado.SelectSingleNode("idEmpleado").InnerText)
+
+
+
+
+
+
+
+            nombre = unEmpleado.SelectSingleNode("nombre").InnerText
+
+
+
+
+            apellidos = unEmpleado.SelectSingleNode("apellidos").InnerText
+
+
+            numeroSS = unEmpleado.SelectSingleNode("numeroSS").InnerText
+
+
+            Dim telefonos As XmlNodeList = unEmpleado.SelectSingleNode("telefonos").ChildNodes
+
+
+            Dim tlfFijo As XmlNode = telefonos(0)
+            Dim tlfMovil As XmlNode = telefonos(1)
+
+            tlfFijoS = tlfFijo.InnerText
+            tlfMovilS = tlfMovil.InnerText
+
+        End Try
+
+    End Sub
+    Public Sub paAlante()
+        cont = cont + 1
+        Try
+            unEmpleado = listaEmpleados.Item(cont)
+
+            'Seleccionamos el id del empleado.
+            Dim idEmpleado As Integer = Convert.ToInt32(unEmpleado.SelectSingleNode("idEmpleado").InnerText)
+
+
+
+
+            Console.WriteLine()
+
+
+            nombre = unEmpleado.SelectSingleNode("nombre").InnerText
+
+
+            apellidos = unEmpleado.SelectSingleNode("apellidos").InnerText
+
+
+            numeroSS = unEmpleado.SelectSingleNode("numeroSS").InnerText
+
+
+            Dim telefonos As XmlNodeList = unEmpleado.SelectSingleNode("telefonos").ChildNodes
+
+
+            Dim tlfFijo As XmlNode = telefonos(0)
+            Dim tlfMovil As XmlNode = telefonos(1)
+
+            tlfFijoS = tlfFijo.InnerText
+            tlfMovilS = tlfMovil.InnerText
+
+        Catch ex As Exception
+            cont = 0
+            MsgBox("No hay datos")
+
+            unEmpleado = listaEmpleados.Item(cont)
+
+            'Seleccionamos el id del empleado.
+            Dim idEmpleado As Integer = Convert.ToInt32(unEmpleado.SelectSingleNode("idEmpleado").InnerText)
+
+
+
+
+            Console.WriteLine()
+
+
+            nombre = unEmpleado.SelectSingleNode("nombre").InnerText
+
+
+            apellidos = unEmpleado.SelectSingleNode("apellidos").InnerText
+
+
+            numeroSS = unEmpleado.SelectSingleNode("numeroSS").InnerText
+
+
+            Dim telefonos As XmlNodeList = unEmpleado.SelectSingleNode("telefonos").ChildNodes
+
+
+            Dim tlfFijo As XmlNode = telefonos(0)
+            Dim tlfMovil As XmlNode = telefonos(1)
+
+            tlfFijoS = tlfFijo.InnerText
+            tlfMovilS = tlfMovil.InnerText
+
+        End Try
+    End Sub
+    Public Sub paAtras()
+        cont = cont - 1
+        Try
+            unEmpleado = listaEmpleados.Item(cont)
+
+            'Seleccionamos el id del empleado.
+            Dim idEmpleado As Integer = Convert.ToInt32(unEmpleado.SelectSingleNode("idEmpleado").InnerText)
+
+
+
+
+            Console.WriteLine()
+
+
+            nombre = unEmpleado.SelectSingleNode("nombre").InnerText
+
+
+            apellidos = unEmpleado.SelectSingleNode("apellidos").InnerText
+
+
+            numeroSS = unEmpleado.SelectSingleNode("numeroSS").InnerText
+
+
+            Dim telefonos As XmlNodeList = unEmpleado.SelectSingleNode("telefonos").ChildNodes
+
+
+            Dim tlfFijo As XmlNode = telefonos(0)
+            Dim tlfMovil As XmlNode = telefonos(1)
+
+            tlfFijoS = tlfFijo.InnerText
+            tlfMovilS = tlfMovil.InnerText
+
+        Catch ex As Exception
+            cont = listaEmpleados.Count - 1
+            MsgBox("No hay datos")
+
+            unEmpleado = listaEmpleados.Item(cont)
+
+            'Seleccionamos el id del empleado.
+            Dim idEmpleado As Integer = Convert.ToInt32(unEmpleado.SelectSingleNode("idEmpleado").InnerText)
+
+
+
+
+            Console.WriteLine()
+
+
+            nombre = unEmpleado.SelectSingleNode("nombre").InnerText
+
+
+            apellidos = unEmpleado.SelectSingleNode("apellidos").InnerText
+
+
+            numeroSS = unEmpleado.SelectSingleNode("numeroSS").InnerText
+
+
+            Dim telefonos As XmlNodeList = unEmpleado.SelectSingleNode("telefonos").ChildNodes
+
+
+            Dim tlfFijo As XmlNode = telefonos(0)
+            Dim tlfMovil As XmlNode = telefonos(1)
+
+            tlfFijoS = tlfFijo.InnerText
+            tlfMovilS = tlfMovil.InnerText
+
+        End Try
+    End Sub
+    Public Sub palPrincipio()
+        cont = 0
+        Try
+            unEmpleado = listaEmpleados.Item(cont)
+
+            'Seleccionamos el id del empleado.
+            Dim idEmpleado As Integer = Convert.ToInt32(unEmpleado.SelectSingleNode("idEmpleado").InnerText)
+
+
+
+
+            Console.WriteLine()
+
+
+            nombre = unEmpleado.SelectSingleNode("nombre").InnerText
+
+
+            apellidos = unEmpleado.SelectSingleNode("apellidos").InnerText
+
+
+            numeroSS = unEmpleado.SelectSingleNode("numeroSS").InnerText
+
+
+            Dim telefonos As XmlNodeList = unEmpleado.SelectSingleNode("telefonos").ChildNodes
+
+
+            Dim tlfFijo As XmlNode = telefonos(0)
+            Dim tlfMovil As XmlNode = telefonos(1)
+
+            tlfFijoS = tlfFijo.InnerText
+            tlfMovilS = tlfMovil.InnerText
+
+        Catch ex As Exception
+            cont = 0
+            MsgBox("No hay datos")
+
+            unEmpleado = listaEmpleados.Item(cont)
+
+            'Seleccionamos el id del empleado.
+            Dim idEmpleado As Integer = Convert.ToInt32(unEmpleado.SelectSingleNode("idEmpleado").InnerText)
+
+
+
+
+            Console.WriteLine()
+
+
+            nombre = unEmpleado.SelectSingleNode("nombre").InnerText
+
+
+            apellidos = unEmpleado.SelectSingleNode("apellidos").InnerText
+
+
+            numeroSS = unEmpleado.SelectSingleNode("numeroSS").InnerText
+
+
+            Dim telefonos As XmlNodeList = unEmpleado.SelectSingleNode("telefonos").ChildNodes
+
+
+            Dim tlfFijo As XmlNode = telefonos(0)
+            Dim tlfMovil As XmlNode = telefonos(1)
+
+            tlfFijoS = tlfFijo.InnerText
+            tlfMovilS = tlfMovil.InnerText
+
+        End Try
+    End Sub
+    Public Sub palFinal()
+        cont = listaEmpleados.Count - 1
+        Try
+            unEmpleado = listaEmpleados.Item(cont)
+
+            'Seleccionamos el id del empleado.
+            Dim idEmpleado As Integer = Convert.ToInt32(unEmpleado.SelectSingleNode("idEmpleado").InnerText)
+
+
+
+
+            Console.WriteLine()
+
+
+            nombre = unEmpleado.SelectSingleNode("nombre").InnerText
+
+
+            apellidos = unEmpleado.SelectSingleNode("apellidos").InnerText
+
+
+            numeroSS = unEmpleado.SelectSingleNode("numeroSS").InnerText
+
+
+            Dim telefonos As XmlNodeList = unEmpleado.SelectSingleNode("telefonos").ChildNodes
+
+
+            Dim tlfFijo As XmlNode = telefonos(0)
+            Dim tlfMovil As XmlNode = telefonos(1)
+
+            tlfFijoS = tlfFijo.InnerText
+            tlfMovilS = tlfMovil.InnerText
+
+        Catch ex As Exception
+            cont = 0
+            MsgBox("No hay datos")
+
+            unEmpleado = listaEmpleados.Item(cont)
+
+            'Seleccionamos el id del empleado.
+            Dim idEmpleado As Integer = Convert.ToInt32(unEmpleado.SelectSingleNode("idEmpleado").InnerText)
+
+
+
+
+            Console.WriteLine()
+
+
+            nombre = unEmpleado.SelectSingleNode("nombre").InnerText
+
+
+            apellidos = unEmpleado.SelectSingleNode("apellidos").InnerText
+
+
+            numeroSS = unEmpleado.SelectSingleNode("numeroSS").InnerText
+
+
+            Dim telefonos As XmlNodeList = unEmpleado.SelectSingleNode("telefonos").ChildNodes
+
+
+            Dim tlfFijo As XmlNode = telefonos(0)
+            Dim tlfMovil As XmlNode = telefonos(1)
+
+            tlfFijoS = tlfFijo.InnerText
+            tlfMovilS = tlfMovil.InnerText
+
+        End Try
+    End Sub
+    Public Function path(camino As String) As Boolean
+        estructura = camino
+        listaEmpleados = documento.SelectNodes(estructura)
+        Return True
+    End Function
+
+End Class
---
+++ WindowsApplication7/Form1.vb
@@ -0,0 +1,85 @@
+﻿
+
+Public Class Form1
+    Dim objeto As New ClassXml
+    Dim tecla As New System.Windows.Forms.Keys()
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Button5.Text = "<<"
+        Button4.Text = ">>"
+        Button6.Text = "build"
+        Label1.Text = "Nombre"
+        Label2.Text = "Apellidos"
+        Label3.Text = "N.S.S"
+        Label4.Text = "Telefono fijo"
+        Label5.Text = "Telefono movil"
+        Button1.Text = "Cargar "
+        Button2.Text = "-->"
+        Button3.Text = "<--"
+        Label6.Text = "[_________]"
+        Label7.Text = "[_________]"
+        Label8.Text = "[_________]"
+        Label9.Text = "[_________]"
+        Label10.Text = "[_________]"
+        Button1.Enabled = False
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        objeto.abrirArchivo(Ruta())
+        carga()
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        objeto.paAlante()
+        carga()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        objeto.paAtras()
+        carga()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        objeto.palFinal()
+        carga()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        objeto.palPrincipio()
+        carga()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If (objeto.path(TextBox1.Text)) Then
+            Button1.Enabled = True
+
+        End If
+    End Sub
+    Private Sub carga()
+        Label6.Text = objeto.nombre
+        Label7.Text = objeto.ape
+        Label8.Text = objeto.SS
+        Label9.Text = objeto.tlff
+        Label10.Text = objeto.tlfm
+
+    End Sub
+    Private Function Ruta() As String
+        Dim file As New OpenFileDialog
+        Dim rut As String = " "
+        file.Filter = "Tipo de arcivo *|*"
+        file.FileName = "Archivo de programa"
+        If (file.ShowDialog = 1) Then
+            rut = file.FileName
+        End If
+        Return rut
+    End Function
+
+
+
+End Class
